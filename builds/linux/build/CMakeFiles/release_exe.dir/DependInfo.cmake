4817545ed5435551770bebe602c467680a4da43b
Pablo Gálvez Ortigosa <86744039+pablogo22@users.noreply.github.com>
2023-09-24 18:29:35 +0200

    INicio

---
+++ builds/linux/build/CMakeFiles/release_exe.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/cauce.cpp" "/home/pablo/Escritorio/Repositorios/opengl3-minimo/builds/linux/build/CMakeFiles/release_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/cauce.cpp.o"
+  "/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/main.cpp" "/home/pablo/Escritorio/Repositorios/opengl3-minimo/builds/linux/build/CMakeFiles/release_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/main.cpp.o"
+  "/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/vaos-vbos.cpp" "/home/pablo/Escritorio/Repositorios/opengl3-minimo/builds/linux/build/CMakeFiles/release_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/vaos-vbos.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
