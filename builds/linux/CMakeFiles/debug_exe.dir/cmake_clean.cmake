11347b7a65b7cf42cf67d26d654f14bd8ef22a5b
Pablo Gálvez Ortigosa <86744039+pablogo22@users.noreply.github.com>
2023-09-25 02:40:43 +0200

    make configurado

---
+++ builds/linux/CMakeFiles/debug_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/pablo/Escritorio/Repositorios/opengl3-minimo/builds/bin/debug_exe"
+  "/home/pablo/Escritorio/Repositorios/opengl3-minimo/builds/bin/debug_exe.pdb"
+  "CMakeFiles/debug_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/cauce.cpp.o"
+  "CMakeFiles/debug_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/main.cpp.o"
+  "CMakeFiles/debug_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/vaos-vbos.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/debug_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ builds/linux/CMakeFiles/release_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/pablo/Escritorio/Repositorios/opengl3-minimo/builds/bin/release_exe"
+  "/home/pablo/Escritorio/Repositorios/opengl3-minimo/builds/bin/release_exe.pdb"
+  "CMakeFiles/release_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/cauce.cpp.o"
+  "CMakeFiles/release_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/main.cpp.o"
+  "CMakeFiles/release_exe.dir/home/pablo/Escritorio/Repositorios/opengl3-minimo/src/vaos-vbos.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/release_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
